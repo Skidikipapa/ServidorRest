5331879695ea94037ba4c19cb81618442779a13b
Aleix Barrera <aleix@localhost.localdomain>
2020-08-12 20:22:25 +0200

    Subir inicialización del proyecto al repositorio

---
+++ src/main/java/es/prueba/web/servidorrest/ServidorrestApplication.java
@@ -0,0 +1,13 @@
+package es.prueba.web.servidorrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorrestApplication.class, args);
+	}
+
+}
